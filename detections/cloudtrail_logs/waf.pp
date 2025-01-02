2c55dd2f1d0dfc42e261742a4d2b924ed1011846
misraved <ved@turbot.com>
2025-01-02 15:07:16 +0530

    Add new detections to WAF

--- detections/cloudtrail_logs/waf.pp
+++ detections/cloudtrail_logs/waf.pp
@@ -3,8 +3,10 @@ locals {
     service = "AWS/WAF"
   })
 
-  cloudtrail_logs_detect_waf_web_acl_deletions_sql_columns       = replace(local.cloudtrail_log_detection_sql_columns, "__RESOURCE_SQL__", "json_extract_string(request_parameters, '$.id')")
-  cloudtrail_logs_detect_waf_web_acl_disassociations_sql_columns = replace(local.cloudtrail_log_detection_sql_columns, "__RESOURCE_SQL__", "json_extract_string(request_parameters, '$.resourceArn')")
+  cloudtrail_logs_detect_waf_acls_with_logging_disabled_sql_columns = replace(local.cloudtrail_log_detection_sql_columns, "__RESOURCE_SQL__", "json_extract_string(request_parameters, '$.resourceArn')")
+  cloudtrail_logs_detect_waf_acl_disassociation_from_cloudfront_distributions_sql_columns = replace(local.cloudtrail_log_detection_sql_columns, "__RESOURCE_SQL__", "json_extract_string(request_parameters, '$.resourceArn')")
+  cloudtrail_logs_detect_waf_acl_disassociation_from_alb_sql_columns = replace(local.cloudtrail_log_detection_sql_columns, "__RESOURCE_SQL__", "json_extract_string(request_parameters, '$.resourceArn')")
+  cloudtrail_logs_detect_public_access_granted_to_waf_rules_sql_columns = replace(local.cloudtrail_log_detection_sql_columns, "__RESOURCE_SQL__", "json_extract_string(request_parameters, '$.ruleArn')")
 }
 
 benchmark "cloudtrail_logs_waf_detections" {
@@ -12,8 +14,10 @@ benchmark "cloudtrail_logs_waf_detections" {
   description = "This benchmark contains recommendations when scanning CloudTrail logs for WAF events."
   type        = "detection"
   children    = [
-    detection.cloudtrail_logs_detect_waf_web_acl_deletions,
-    detection.cloudtrail_logs_detect_waf_web_acl_disassociations,
+    detection.cloudtrail_logs_detect_waf_acl_disassociation_from_alb,
+    detection.cloudtrail_logs_detect_waf_acl_disassociation_from_cloudfront_distributions,
+    detection.cloudtrail_logs_detect_waf_acls_with_logging_disabled,
+    detection.cloudtrail_logs_detect_public_access_granted_to_waf_rules,
   ]
 
   tags = merge(local.cloudtrail_log_detection_waf_common_tags, {
@@ -21,54 +25,112 @@ benchmark "cloudtrail_logs_waf_detections" {
   })
 }
 
-detection "cloudtrail_logs_detect_waf_web_acl_deletions" {
-  title           = "Detect WAF Web ACL Deletions"
-  description     = "Detect when a WAF web ACL is deleted to check for potential disruptions to web application protections, which could expose applications to malicious traffic or DDoS attacks."
-  severity        = "medium"
+detection "cloudtrail_logs_detect_waf_acls_with_logging_disabled" {
+  title           = "Detect WAF Web ACLs with Logging Disabled"
+  description     = "Detect WAF Web ACLs with logging disabled to check for changes that could hinder monitoring and auditing, potentially obscuring malicious activity or misconfigurations."
+  severity        = "high"
   display_columns = local.cloudtrail_log_detection_display_columns
-  query           = query.cloudtrail_logs_detect_waf_web_acl_deletions
-
-  tags = merge(local.cloudtrail_log_detection_waf_common_tags, {
-    mitre_attack_ids = "TA0005:T1562.001"
-  })
-}
-
-detection "cloudtrail_logs_detect_waf_web_acl_disassociations" {
-  title           = "Detect WAF Web ACL Disassociations"
-  description     = "Detect when a WAF web ACL is disassociated from a resource to check for intentional bypassing of security protections or misconfigurations that could allow unrestricted access to web applications."
-  severity        = "medium"
-  display_columns = local.cloudtrail_log_detection_display_columns
-  query           = query.cloudtrail_logs_detect_waf_web_acl_disassociations
+  query           = query.cloudtrail_logs_detect_waf_acls_with_logging_disabled
 
   tags = merge(local.cloudtrail_log_detection_common_tags, {
-    mitre_attack_ids = "TA0004:T1498"
+    mitre_attack_ids = "TA0005:T1562.002"
   })
 }
 
-query "cloudtrail_logs_detect_waf_web_acl_deletions" {
+query "cloudtrail_logs_detect_waf_acls_with_logging_disabled" {
   sql = <<-EOQ
     select
-      ${local.cloudtrail_logs_detect_waf_web_acl_deletions_sql_columns}
+      ${local.cloudtrail_logs_detect_waf_acls_with_logging_disabled_sql_columns}
     from
       aws_cloudtrail_log
     where
-      event_source in ('waf.amazonaws.com', 'wafv2.amazonaws.com')
-      and event_name = 'DeleteWebACL'
+      event_source = 'wafv2.amazonaws.com'
+      and event_name in ('DeleteLoggingConfiguration', 'PutLoggingConfiguration')
+      and json_extract_string(request_parameters, '$.loggingConfiguration') is null
       ${local.cloudtrail_log_detections_where_conditions}
     order by
       event_time desc;
   EOQ
 }
 
-query "cloudtrail_logs_detect_waf_web_acl_disassociations" {
+detection "cloudtrail_logs_detect_waf_acl_disassociation_from_cloudfront_distributions" {
+  title           = "Detect WAF Web ACL Disassociation from CloudFront Distributions"
+  description     = "Detect when a WAF Web ACL is disassociated from CloudFront distributions, potentially exposing them to unauthorized access or attacks."
+  severity        = "high"
+  display_columns = local.cloudtrail_log_detection_display_columns
+  query           = query.cloudtrail_logs_detect_waf_acl_disassociation_from_cloudfront_distributions
+
+  tags = merge(local.cloudtrail_log_detection_common_tags, {
+    mitre_attack_ids = "TA0005:T1562.001"
+  })
+}
+
+query "cloudtrail_logs_detect_waf_acl_disassociation_from_cloudfront_distributions" {
   sql = <<-EOQ
     select
-      ${local.cloudtrail_logs_detect_waf_web_acl_disassociations_sql_columns}
+      ${local.cloudtrail_logs_detect_waf_acl_disassociation_from_cloudfront_distributions_sql_columns}
     from
       aws_cloudtrail_log
     where
       event_source = 'wafv2.amazonaws.com'
       and event_name = 'DisassociateWebACL'
+      and json_extract_string(request_parameters, '$.resourceArn') like '%cloudfront::%:distribution/%'
+      ${local.cloudtrail_log_detections_where_conditions}
+    order by
+      event_time desc;
+  EOQ
+}
+
+detection "cloudtrail_logs_detect_waf_acl_disassociation_from_alb" {
+  title           = "Detect WAF Web ACL Disassociation from Application Load Balancers"
+  description     = "Detect when a WAF Web ACL is disassociated from Application Load Balancers (ALBs), potentially exposing them to unauthorized access or attacks."
+  severity        = "high"
+  display_columns = local.cloudtrail_log_detection_display_columns
+  query           = query.cloudtrail_logs_detect_waf_acl_disassociation_from_alb
+
+  tags = merge(local.cloudtrail_log_detection_common_tags, {
+    mitre_attack_ids = "TA0005:T1562.001"
+  })
+}
+
+query "cloudtrail_logs_detect_waf_acl_disassociation_from_alb" {
+  sql = <<-EOQ
+    select
+      ${local.cloudtrail_logs_detect_waf_acl_disassociation_from_alb_sql_columns}
+    from
+      aws_cloudtrail_log
+    where
+      event_source = 'wafv2.amazonaws.com'
+      and event_name = 'DisassociateWebACL'
+      and json_extract_string(request_parameters, '$.resourceArn') like '%elasticloadbalancing:%:%:loadbalancer/app/%'
+      ${local.cloudtrail_log_detections_where_conditions}
+    order by
+      event_time desc;
+  EOQ
+}
+
+detection "cloudtrail_logs_detect_public_access_granted_to_waf_rules" {
+  title           = "Detect Public Access Granted to WAF Rules"
+  description     = "Detect when a WAF rule is configured to allow unrestricted public access (0.0.0.0/0), potentially exposing protected resources to unauthorized access or attacks."
+  severity        = "high"
+  display_columns = local.cloudtrail_log_detection_display_columns
+  query           = query.cloudtrail_logs_detect_public_access_granted_to_waf_rules
+
+  tags = merge(local.cloudtrail_log_detection_common_tags, {
+    mitre_attack_ids = "TA0001:T1190"
+  })
+}
+
+query "cloudtrail_logs_detect_public_access_granted_to_waf_rules" {
+  sql = <<-EOQ
+    select
+      ${local.cloudtrail_logs_detect_public_access_granted_to_waf_rules_sql_columns}
+    from
+      aws_cloudtrail_log
+    where
+      event_source = 'wafv2.amazonaws.com'
+      and event_name in ('UpdateRule', 'PutRule')
+      and json_extract_string(request_parameters, '$.rules') like '%0.0.0.0/0%'
       ${local.cloudtrail_log_detections_where_conditions}
     order by
       event_time desc;
