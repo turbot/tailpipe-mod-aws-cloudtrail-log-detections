141aee0381a95c127d39e59a8cff1b2c847f0c44
ParthaI <partha@turbot.com>
2025-01-13 16:55:36 +0530

    Added documentation links for the cloudtrail logs

--- detections/cloudtrail.pp
+++ detections/cloudtrail.pp
@@ -28,6 +28,7 @@ benchmark "cloudtrail_detections" {
 detection "detect_cloudtrail_trails_with_logging_stopped" {
   title           = "Detect CloudTrail Trails with Logging Stopped"
   description     = "Detect CloudTrail trails with logging stopped to check for changes that could reduce visibility into critical AWS API activity, potentially obscuring unauthorized access or configuration changes."
+  documentation   = file("./detections/docs/detect_cloudtrail_trails_with_logging_stopped.md")
   severity        = "high"
   display_columns = local.detection_display_columns
   query           = query.detect_cloudtrail_trails_with_logging_stopped
@@ -56,6 +57,7 @@ query "detect_cloudtrail_trails_with_logging_stopped" {
 detection "detect_cloudtrail_trails_with_s3_logging_disabled" {
   title           = "Detect CloudTrail Trails with S3 Logging Disabled"
   description     = "Detect CloudTrail trails with S3 logging disabled to check for changes that could reduce visibility into critical S3 data events, hindering the ability to detect unauthorized access or data exfiltration."
+  documentation   = file("./detections/docs/detect_cloudtrail_trails_with_s3_logging_disabled.md")
   severity        = "medium"
   display_columns = local.detection_display_columns
   query           = query.detect_cloudtrail_trails_with_s3_logging_disabled
@@ -84,6 +86,7 @@ query "detect_cloudtrail_trails_with_s3_logging_disabled" {
 detection "detect_cloudtrail_trails_with_lambda_logging_disabled" {
   title           = "Detect CloudTrail Trails with Lambda Logging Disabled"
   description     = "Detect CloudTrail trails with Lambda logging disabled to check for changes that could reduce visibility into Lambda invocation events, potentially obscuring unauthorized activity or misconfigurations."
+  documentation   = file("./detections/docs/detect_cloudtrail_trails_with_lambda_logging_disabled.md")
   severity        = "medium"
   display_columns = local.detection_display_columns
   query           = query.detect_cloudtrail_trails_with_lambda_logging_disabled
@@ -112,6 +115,7 @@ query "detect_cloudtrail_trails_with_lambda_logging_disabled" {
 detection "detect_cloudtrail_trails_with_encryption_disabled" {
   title           = "Detect CloudTrail Trails with Encryption Disabled"
   description     = "Detect CloudTrail trails with encryption disabled to check for events where KMS keys are removed, potentially exposing sensitive log data to unauthorized access or tampering."
+  documentation   = file("./detections/docs/detect_cloudtrail_trails_with_encryption_disabled.md")
   severity        = "high"
   display_columns = local.detection_display_columns
   query           = query.detect_cloudtrail_trails_with_encryption_disabled
@@ -141,6 +145,7 @@ query "detect_cloudtrail_trails_with_encryption_disabled" {
 detection "detect_cloudtrail_trails_with_kms_key_updated" {
   title           = "Detect CloudTrail Trails with KMS Key Updated"
   description     = "Detect changes to the KMS key used for encrypting CloudTrail logs to check for potential misconfigurations or unauthorized updates that could redirect log encryption to an untrusted or compromised key."
+  documentation   = file("./detections/docs/detect_cloudtrail_trails_with_kms_key_updated.md")
   severity        = "high"
   display_columns = local.detection_display_columns
   query           = query.detect_cloudtrail_trails_with_kms_key_updated
@@ -170,6 +175,7 @@ query "detect_cloudtrail_trails_with_kms_key_updated" {
 detection "detect_cloudtrail_trails_with_s3_logging_bucket_modified" {
   title           = "Detect CloudTrail Trails with S3 Logging Bucket Modified"
   description     = "Detect changes to the S3 bucket used for storing CloudTrail logs to check for events that could redirect log data to an unauthorized or insecure location, compromising log integrity and security."
+  documentation   = file("./detections/docs/detect_cloudtrail_trails_with_s3_logging_bucket_modified.md")
   severity        = "medium"
   display_columns = local.detection_display_columns
   query           = query.detect_cloudtrail_trails_with_s3_logging_bucket_modified
@@ -199,6 +205,7 @@ query "detect_cloudtrail_trails_with_s3_logging_bucket_modified" {
 detection "detect_cloudtrail_trails_with_global_service_logging_disabled" {
   title           = "Detect CloudTrail Trails with Global Service Logging Disabled"
   description     = "Detect CloudTrail trails with global service logging disabled to check for changes that could reduce visibility into critical global service activity, potentially hindering threat detection and compliance efforts."
+  documentation   = file("./detections/docs/detect_cloudtrail_trails_with_global_service_logging_disabled.md")
   severity        = "high"
   display_columns = local.detection_display_columns
   query           = query.detect_cloudtrail_trails_with_global_service_logging_disabled
