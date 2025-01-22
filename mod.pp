27b006c366636f4b757f15d9014a080e7c39af0d
misraved <ved@turbot.com>
2025-01-22 18:03:31 +0530

    Added the icon reference for the AWS detection

--- mod.pp
+++ mod.pp
@@ -4,7 +4,7 @@ mod "aws_cloudtrail_log_detections" {
   description   = "Search your AWS CloudTrail logs for high risk actions using Tailpipe."
   color         = "#FF9900"
   documentation = file("./docs/index.md")
-  #icon          = "/images/mods/turbot/aws.svg"
+  icon          = "/images/mods/turbot/aws-detections.svg"
   categories    = ["aws", "dashboard", "detections", "public cloud"]
   database      = var.database
 
