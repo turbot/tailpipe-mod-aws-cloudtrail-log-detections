0e5087058f3ab87a5d406a0c6483034d3df13d4f
Cody Bruno <cody@turbot.com>
2025-02-25 16:53:06 -0500

    Test using inputs in reports with 'all' value

--- dashboards/root_user_activity_report.pp
+++ dashboards/root_user_activity_report.pp
@@ -13,13 +13,14 @@ dashboard "root_user_activity_report" {
       title = "Include read-only events:"
       width = 2
 
+      option "false" {
+        label = "No"
+      }
+
       option "true" {
         label = "Yes"
       }
 
-      option "false" {
-        label = "No"
-      }
     }
 
     input "aws_accounts" {
@@ -28,6 +29,28 @@ dashboard "root_user_activity_report" {
       type  = "multiselect"
       width = 2
     }
+
+    input "event_names" {
+      title = "Select event names:"
+      query = query.root_user_activity_report_event_names_input
+      type  = "multiselect"
+      width = 2
+    }
+
+    input "source_ip_addresses" {
+      title = "Select source IPs:"
+      query = query.root_user_activity_report_source_ip_addresses_input
+      type  = "multiselect"
+      width = 2
+    }
+
+    input "event_version" {
+      title = "Select event version:"
+      query = query.root_user_activity_report_event_version_input
+      type  = "select"
+      width = 2
+    }
+
   }
 
   container {
@@ -36,7 +59,10 @@ dashboard "root_user_activity_report" {
       width = 2
       args = [
         self.input.read_only.value,
-        self.input.aws_accounts.value
+        self.input.aws_accounts.value,
+        self.input.event_names.value,
+        self.input.source_ip_addresses.value,
+        self.input.event_version.value
       ]
     }
   }
@@ -47,7 +73,10 @@ dashboard "root_user_activity_report" {
       query = query.root_user_activity_report_table
       args = [
         self.input.read_only.value,
-        self.input.aws_accounts.value
+        self.input.aws_accounts.value,
+        self.input.event_names.value,
+        self.input.source_ip_addresses.value,
+        self.input.event_version.value
       ]
     }
   }
@@ -62,8 +91,11 @@ query "root_user_activity_report_total_logs" {
       aws_cloudtrail_log
     where
       ($1 = 'true' or ($1 = 'false' and read_only = false))
-      and recipient_account_id in $2
-      and user_identity.type = 'Root'
+      and ('all' in $2 or (recipient_account_id in $2))
+      and ('all' in $3 or (event_name in $3))
+      and ('all' in $4 or (source_ip_address in $4))
+      and (event_version = 'all' or (event_version in $5))
+      --and user_identity.type = 'Root'
   EOQ
 }
 
@@ -82,11 +114,14 @@ query "root_user_activity_report_table" {
       aws_cloudtrail_log
     where
       ($1 = 'true' or ($1 = 'false' and read_only = false))
-      and recipient_account_id in $2
-      and user_identity.type = 'Root'
+      and ('all' in $2 or (recipient_account_id in $2))
+      and ('all' in $3 or (event_name in $3))
+      and ('all' in $4 or (source_ip_address in $4))
+      and (event_version = 'all' or (event_version in $5))
+      --and user_identity.type = 'Root'
     order by
       timestamp desc
-    limit 10000;
+    limit 100;
   EOQ
 }
 
@@ -100,15 +135,101 @@ query "root_user_activity_report_aws_accounts_input" {
       select
         distinct(recipient_account_id) as aws_account_id
       from
-      aws_cloudtrail_log
+        aws_cloudtrail_log
     )
+    select
+      'All' as label,
+      'all' as value
+    union all
     select
       aws_account_id as label,
       aws_account_id as value
     from
       aws_account_ids
-    order by
-      aws_account_id;
+  EOQ
+
+  tags = {
+    folder = "Internal"
+  }
+}
+
+query "root_user_activity_report_event_names_input" {
+  title = "Root User Activity Report Event Names Input"
+
+  sql = <<-EOQ
+    with event_names as (
+      select
+        distinct(event_name) as event_name
+      from
+        aws_cloudtrail_log
+      order by
+        event_name
+    )
+    select
+      'All' as label,
+      'all' as value
+    union all
+    select
+      event_name as label,
+      event_name as value
+    from
+      event_names
+  EOQ
+
+  tags = {
+    folder = "Internal"
+  }
+}
+
+query "root_user_activity_report_source_ip_addresses_input" {
+  title = "Root User Activity Report Source IP Addresses Input"
+
+  sql = <<-EOQ
+    with source_ip_addresses as (
+      select
+        distinct(source_ip_address) as source_ip_address
+      from
+        aws_cloudtrail_log
+      order by
+        source_ip_address
+    )
+    select
+      'All' as label,
+      'all' as value
+    union all
+    select
+      source_ip_address as label,
+      source_ip_address as value
+    from
+      source_ip_addresses
+  EOQ
+
+  tags = {
+    folder = "Internal"
+  }
+}
+
+query "root_user_activity_report_event_version_input" {
+  title = "Root User Activity Report Event Version Input"
+
+  sql = <<-EOQ
+    with event_versions as (
+      select
+        distinct(event_version) as event_version
+      from
+        aws_cloudtrail_log
+      order by
+        event_version
+    )
+    select
+      'All' as label,
+      'all' as value
+    union all
+    select
+      event_version as label,
+      event_version as value
+    from
+      event_versions
   EOQ
 
   tags = {
